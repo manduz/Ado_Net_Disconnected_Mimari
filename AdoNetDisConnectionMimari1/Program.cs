7d6bd7f0d7247026fbdd42e589a8101772e97caf
manduz <emekmanduz3623@gmail.com>
2024-11-13 14:26:41 +0300

    dataadapter a nasıl  aktarılıyor

---
+++ AdoNetDisConnectionMimari1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetDisConnectionMimari1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-CU1M14V\\SQLKODLAB;Initial Catalog=SQLFULL;Persist Security Info=True;User ID=udemy1;Password=1;Encrypt=True;TrustServerCertificate=True");
+           
+            SqlDataAdapter adapter = new SqlDataAdapter("select * from Musterim; select * from MusteriGirisBilgileri", con);
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+
+           
+
+
+        }
+    }
+}
